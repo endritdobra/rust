a1c5f811c33df6e28f9476cbc7def535f0d19c8c
endritdobra <endrit.dobra@gmail.com>
2025-12-27 09:19:55 +0100

    Add `Account` and `Bank` structs with basic functionality

--- bank/src/main.rs
+++ bank/src/main.rs
@@ -1,7 +1,38 @@
+#[derive(Debug)]
 struct Account {
+    id: i32,
     balance: i32,
+    holder: String,
+}
+
+#[derive(Debug)]
+struct Bank {
+    accounts: Vec<Account>,
+}
+
+impl Account {
+    fn new(id: i32, holder: String) -> Self {
+        Self { id, holder, balance: 0 }
+    }
+}
+
+impl Bank {
+    fn new() -> Self {
+        Self { accounts: vec![] }
+    }
+}
+
+fn print_bank(bank: Bank) {
+    println!("Bank: {:#?}", bank);
+}
+
+fn print_accounts(accounts: Vec<Account>) {
+    println!("Account: {:#?}", accounts);
 }
 
 fn main() {
-    println!("Hello, world!");
+    let bank = Bank::new();
+    let account = Account::new(1, "John".to_string());
+
+    print_accounts(bank.accounts);
 }
